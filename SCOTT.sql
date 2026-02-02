39cadf098c7bebe067e1f2dd70d2ea2a2437017d
jaesung <swotjd9805@naver.com>
2026-02-02 10:21:43 +0900

    첫번째 커밋

---
+++ SCOTT.sql
@@ -0,0 +1,10 @@
+-- Oracle(DBMS) + version(21c) xe(database명)
+-- user(scott) - 테이블.
+-- Structured Query Language (SQL)
+SELECT studno, name -- 컬럼명(전체 지목)
+FROM student; -- 테이블
+
+
+-- 1. professor 테이블. 전체 컬럼 조회.
+SELECT * 
+FROM professor
