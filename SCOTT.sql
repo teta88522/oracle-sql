a611a21865002ddcfbf41e04a7833fb36cb3a832
jaesung <swotjd9805@naver.com>
2026-02-02 10:47:42 +0900

    쿼리 수정

--- SCOTT.sql
+++ SCOTT.sql
@@ -1,10 +1,14 @@
 -- Oracle(DBMS) + version(21c) xe(database명)
 -- user(scott) - 테이블.
 -- Structured Query Language (SQL)
-SELECT studno, name -- 컬럼명(전체 지목)
+SELECT * -- 컬럼명(전체 지목)
 FROM student; -- 테이블
 
 
 -- 1. professor 테이블. 전체 컬럼 조회.
 SELECT * 
-FROM professor
+FROM professor;
+
+-- 2) 학생 --> 학생번호,이름,학년
+SELECT studno,name,grade
+FROM student;
