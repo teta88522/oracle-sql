e003bf09ad25236d6e0abd495d25f0dee5dd33f9
jaesung <swotjd9805@naver.com>
2026-02-03 17:48:04 +0900

    '2일차'

--- todo.sql
+++ todo.sql
@@ -6,3 +6,21 @@ FROM emp;
  --                                 4~6 => 1/4 분기
   --                                7~9 => 1/4 분기
     --                              10~12 => 1/4 분기
+    
+    
+-- 107page
+SELECT empno,
+ename,
+sal,
+comm,
+to_char(sal*12+comm,'99,999') SALARY
+FROM emp
+WHERE ename = 'ALLEN';
+
+SELECT name,
+pay,
+bonus,
+to_char(pay*12+bonus,'99,999') TOTAL
+FROM professor 
+WHERE deptno = 201;
+
