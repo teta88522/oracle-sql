30b50bef2509da18fae5ed4223a234ebd32ac498
js <swotjd9805@naver.com>
2026-02-03 23:20:44 +0900

    '2day'

--- todo.sql
+++ todo.sql
@@ -1,11 +1,6 @@
 SELECT ename || '''s ' || 'sal is $' || sal
 FROM emp;
 
--- 2026 2 03  107 108 113 page
--- 학생테이블의 생년월일을 기준으로 1~3 => 1/4 분기 = 이런식으로 출력
- --                                 4~6 => 1/4 분기
-  --                                7~9 => 1/4 분기
-    --                              10~12 => 1/4 분기
     
     
 -- 107page
@@ -24,3 +19,27 @@ to_char(pay*12+bonus,'99,999') TOTAL
 FROM professor 
 WHERE deptno = 201;
 
+-- 108page
+SELECT empno,
+ename,
+hiredate,
+sal,
+'$'|| to_char((sal*12+comm)*1.15,'99,999') "15%up"
+FROM emp
+WHERE comm is not null;
+
+-- 113page
+SELECT empno,
+ENAME,
+comm,
+NVL2(comm,'Exist',NULL)
+FROM emp
+WHERE deptno = 30;
+
+-- 학생테이블의 생년월일을 기준으로 1~3 => 1/4 분기 = 이런식으로 출력
+ --                                 4~6 => 1/4 분기
+  --                                7~9 => 1/4 분기
+    --                              10~12 => 1/4 분기
+    
+    
+    
